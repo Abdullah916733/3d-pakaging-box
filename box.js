b10a1480c4914444170ca184f997d1e3fa9eba84
narendra2895 <106621535+narendra2895@users.noreply.github.com>
2023-06-23 10:25:26 +0530

    box edges fixed

--- box.js
+++ box.js
@@ -1,13 +1,3 @@
-
-function loadFallbackThreeJS() {
-    var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/three@0.130.0/build/three.min.js';
-    script.onload = function() {
-        initializeScene();
-    };
-    document.head.appendChild(script);
-}
-
 function initializeScene() {
     // Get the container element
     const container = document.getElementById('cube-container');
@@ -15,7 +5,7 @@ function initializeScene() {
     // Create a scene, camera, and renderer using Three.js
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(container.clientWidth, container.clientHeight);
     renderer.setClearColor(0xf7f7f7);
 
@@ -27,52 +17,96 @@ function initializeScene() {
     const depthInput = document.getElementById('depth');
     const colorInput = document.getElementById('color');
 
-    // Create a cube geometry and material
-    const cubeGeometry = new THREE.BoxGeometry(Number(widthInput.value), Number(heightInput.value), Number(
-        depthInput.value));
-    const cubeMaterial = new THREE.MeshBasicMaterial({
+    // Set default values for the inputs
+    widthInput.value = '100';
+    heightInput.value = '100';
+    depthInput.value = '100';
+    colorInput.value = '#1d1d1d'; // Set the default color to black
+
+    // Update the cube's dimensions and color when the form is submitted
+    document.getElementById('saveButton').addEventListener('click', updateCube);
+
+    // Create a cube geometry with default values
+    const cubeGeometry = new THREE.BoxGeometry(Number(widthInput.value), Number(heightInput.value), Number(depthInput.value));
+
+    // Create a face material using MeshBasicMaterial with input color
+    const faceMaterial = new THREE.MeshBasicMaterial({
         color: colorInput.value
     });
 
-    // Create a cube mesh and add it to the scene
-    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    scene.add(cube);
+    // Create a wireframe material using LineBasicMaterial with white color
+    const wireframeMaterial = new THREE.LineBasicMaterial({
+        color: 0xffffff
+    });
+
+    // Create a cube mesh with face material
+    const cube = new THREE.Mesh(cubeGeometry, faceMaterial);
+
+    // Create a wireframe geometry using EdgesGeometry and cube geometry
+    const wireframeGeometry = new THREE.EdgesGeometry(cubeGeometry);
+
+    // Create a wireframe mesh using wireframe geometry and wireframe material
+    const wireframe = new THREE.LineSegments(wireframeGeometry, wireframeMaterial);
+    cube.add(wireframe); // Add wireframe as a child of the cube
+
+    scene.add(cube); // Add cube to the scene
 
     // Set camera position and add it to the scene
     camera.position.z = 300;
     scene.add(camera);
 
-    // Update the cube's dimensions and color when the form is submitted
-    document.getElementById('saveButton').addEventListener('click', updateCube);
+    let isDragging = false;
+    let previousMousePosition = {
+        x: 0,
+        y: 0
+    };
 
     function updateCube() {
-        cube.geometry = new THREE.BoxGeometry(Number(widthInput.value), Number(heightInput.value), Number(depthInput
-            .value));
+        cube.geometry = new THREE.BoxGeometry(Number(widthInput.value), Number(heightInput.value), Number(depthInput.value));
         cube.material.color.set(colorInput.value);
     }
 
     // Render the scene with the camera
     function render() {
         requestAnimationFrame(render);
-      
+
         renderer.render(scene, camera);
     }
     render();
 
-    // Update the cube's dimensions and color when the form is submitted
-    function updateCube() {
-        cube.geometry = new THREE.BoxGeometry(Number(widthInput.value), Number(heightInput.value), Number(depthInput
-            .value));
-        cube.material.color.set(colorInput.value);
-    }
+    // Rotate the cube on mouse click and drag
+    container.addEventListener('mousedown', function(event) {
+        isDragging = true;
+        previousMousePosition = {
+            x: event.clientX,
+            y: event.clientY
+        };
+    });
 
-    // Rotate the cube on mouseover
     container.addEventListener('mousemove', function(event) {
-        const mouseX = event.clientX - container.offsetLeft;
-        const mouseY = event.clientY - container.offsetTop;
+        if (isDragging) {
+            const mouseDelta = {
+                x: event.clientX - previousMousePosition.x,
+                y: event.clientY - previousMousePosition.y
+            };
 
-        cube.rotation.x = (mouseY - container.clientHeight / 2) * 0.001;
-        cube.rotation.y = (mouseX - container.clientWidth / 2) * 0.001;
+            cube.rotation.x += mouseDelta.y * 0.01;
+            cube.rotation.y += mouseDelta.x * 0.01;
+
+            previousMousePosition = {
+                x: event.clientX,
+                y: event.clientY
+            };
+        }
+    });
+
+    container.addEventListener('mouseup', function() {
+        isDragging = false;
+    });
+
+    // Disable context menu on right-click to prevent interference with mouse drag
+    container.addEventListener('contextmenu', function(event) {
+        event.preventDefault();
     });
 }
 
