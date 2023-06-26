547d7d11e0604c9b8f73ba80c7e843e335331390
narendra2895 <106621535+narendra2895@users.noreply.github.com>
2023-06-26 14:03:27 +0530

    faces change made

--- box.js
+++ box.js
@@ -5,12 +5,13 @@ function initializeScene() {
     // Get the container element
     const container = document.getElementById('cube-container');
 
+
     // Create a scene, camera, and renderer using Three.js
-    const scene = new THREE.Scene();
+    const scene = new THREE.Scene();   //A scene in Three.js represents a container or environment where you can place and manipulate objects, lights, cameras, and other elements of a 3D scene.
     const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(container.clientWidth, container.clientHeight);
-    renderer.setClearColor(0xf7f7f7);
+    renderer.setClearColor(0x808080);
     renderer.shadowMap.enabled = true; // Enable shadow mapping
     renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Set shadow map type for smoother shadows
 
@@ -25,6 +26,15 @@ function initializeScene() {
     let heightInput = document.getElementById('height');
     let depthInput = document.getElementById('depth');
     const colorInput = document.getElementById('color');
+    const updateButton = document.getElementById('updateColorButton');
+
+    updateButton.addEventListener('click', function () {
+        updateCube();
+    });
+    colorInput.addEventListener('change', function () {
+        const colorValue = colorInput.value;
+        updateCube(colorValue);
+    });
     const saveButton = document.getElementById('saveButton');
 
     // Disable all inputs initially
@@ -129,6 +139,8 @@ function initializeScene() {
     depthInput.value = '200';
     colorInput.value = '#e7e7e7'; // Set the default color to black
 
+
+
     // Update the cube's dimensions and color when the form is submitted
     document.getElementById('saveButton').addEventListener('click', () => {
         // Update the cube's dimensions based on the form inputs
@@ -172,6 +184,7 @@ function initializeScene() {
 
     faceButtons.forEach((button) => {
         button.addEventListener('click', (event) => {
+
             const face = event.target.dataset.face;
 
             // Remove active class from all buttons
@@ -184,42 +197,91 @@ function initializeScene() {
             const rotation = getRotationByFace(face);
             cube.rotation.x = rotation.x;
             cube.rotation.y = rotation.y;
+
         });
     });
 
     // Define rotation values for each face
+    // Define rotation values for each face
     function getRotationByFace(face) {
-        switch (face) {
-          case 'top':
-            return { x: Math.PI / 9, y: Math.PI / 4 };
-          case 'bottom':
-            return { x: Math.PI / 6, y: Math.PI / 4};
-          case 'front':
-            return { x: Math.PI / 6, y: Math.PI / 4 };
-          case 'back':
-            return { x: Math.PI / 6, y: Math.PI / 4 };
-          case 'left':
-            return { x: Math.PI / 6, y: Math.PI / 4 };
-          case 'right':
-            return { x: Math.PI / 6, y: Math.PI / 4 };
-          default:
-            return { x: Math.PI / 6, y: Math.PI / 4 };
+        const duration = 1000; // Duration of the rotation animation in milliseconds
+        const startRotation = { x: cube.rotation.x, y: cube.rotation.y }; // Initial rotation values
+        const targetRotation = getTargetRotation(face); // Get the target rotation values based on the selected face
+
+        let startTime = null;
+
+        function animateRotation(timestamp) {
+            if (!startTime) startTime = timestamp;
+
+            const progress = timestamp - startTime;
+            const t = Math.min(progress / duration, 1); // Calculate the progress of the animation as a value between 0 and 1
+
+            cube.rotation.x = startRotation.x + (targetRotation.x - startRotation.x) * t;
+            cube.rotation.y = startRotation.y + (targetRotation.y - startRotation.y) * t;
+
+            if (progress < duration) {
+                requestAnimationFrame(animateRotation);
+            }
+
         }
-      }
-      
+
+        requestAnimationFrame(animateRotation);
+        return targetRotation;
+    }
+
+    // Helper function to get the target rotation values based on the selected face
+    function getTargetRotation(face) {
+        switch (face) {
+            case 'top':
+                const rotationTY = THREE.MathUtils.degToRad(30);
+                const rotationTX = THREE.MathUtils.degToRad(-30);
+                console.log(face)
+                return { x: Math.PI / 2 + rotationTX, y: rotationTY };
+
+            case 'bottom':
+                const rotation = THREE.MathUtils.degToRad(30);
+                return { x: -Math.PI / 2 + rotation, y: rotation };
+
+            case 'front':
+                return { x: 0.1745, y: 0.3491 }; // 20 degrees in radians (Math.PI / 9)
+            case 'back':
+                return { x: 0.1745, y: Math.PI - 0.3491 }; // -20 degrees in radians (Math.PI - Math.PI / 9)
+            case 'left':
+                return { x: 0.1745, y: Math.PI / 9 * 4 }; // 20 degrees in radians (Math.PI / 9 * 4)
+            case 'right':
+                return { x: 0.1745, y: -Math.PI / 9 * 4 }; // -20 degrees in radians (-Math.PI / 9 * 4)
+            default:
+                return { x: 0, y: 0 };
+        }
+    }
+
+
+
+    //   change face color 
+    // Get the color picker input element and the change color button
 
 
 
     // Create a cube geometry with default values
-    const cubeGeometry = new THREE.BoxGeometry(Number(widthInput.value), Number(heightInput.value), Number(depthInput.value));
+    const cubeGeometry = new THREE.BoxGeometry(
+        Number(widthInput.value),
+        Number(heightInput.value),
+        Number(depthInput.value)
+    );
 
-    // Create a face material using MeshLambertMaterial with input color
-    const faceMaterial = new THREE.MeshLambertMaterial({
-        color: colorInput.value
-    });
+    // Create an array of materials for each face
+    const faceMaterials = [
+        new THREE.MeshLambertMaterial({ color: 0xe7e7e7 }), // Red material for the front face   0
+        new THREE.MeshLambertMaterial({ color: 0xe7e7e7 }), // Green material for the back face   1 
+        new THREE.MeshLambertMaterial({ color: 0xe7e7e7 }), // Blue material for the top face  2
+        new THREE.MeshLambertMaterial({ color: 0xe7e7e7 }), // Yellow material for the bottom face  3
+        new THREE.MeshLambertMaterial({ color: 0xe7e7e7 }), // Magenta material for the right face  4 
+        new THREE.MeshLambertMaterial({ color: 0xe7e7e7 }) // Cyan material for the left face   5
+    ];
 
-    // Create a cube mesh with face material
-    const cube = new THREE.Mesh(cubeGeometry, faceMaterial);
+    // Create a cube mesh with different materials for each face
+    const cube = new THREE.Mesh(cubeGeometry, faceMaterials);
+
     cube.castShadow = true; // Enable the cube to cast shadows
     cube.receiveShadow = true; // Allow the cube's material to receive shadows
     scene.add(cube); // Add cube to the scene
@@ -229,9 +291,34 @@ function initializeScene() {
     camera.lookAt(scene.position);
     scene.add(camera);
 
+
+    //this is the function to animate the generated cube by rotating it a bit at the start
     // Set the initial rotation of the cube
-    cube.rotation.y = Math.PI / 4; // 45 degrees in radians
-    cube.rotation.x = Math.PI / 6; // 20 degrees in radians
+    const targetRotationY = Math.PI / 4; // 45 degrees in radians
+    const targetRotationX = Math.PI / 6; // 20 degrees in radians
+
+    const startRotation = { x: 0, y: 0 };
+    const targetRotation = { x: targetRotationX, y: targetRotationY };
+    const duration = 1000; // Animation duration in milliseconds
+
+    let startTime = null;
+
+    function animateInitialRotation(timestamp) {
+        if (!startTime) startTime = timestamp;
+
+        const progress = timestamp - startTime;
+        const t = Math.min(progress / duration, 1); // Calculate the progress of the animation as a value between 0 and 1
+
+        cube.rotation.x = startRotation.x + (targetRotation.x - startRotation.x) * t;
+        cube.rotation.y = startRotation.y + (targetRotation.y - startRotation.y) * t;
+
+        if (progress < duration) {
+            requestAnimationFrame(animateInitialRotation);
+        }
+    }
+
+    requestAnimationFrame(animateInitialRotation);
+
 
 
     // Create a directional light from top right and back position
@@ -258,13 +345,47 @@ function initializeScene() {
         renderer.render(scene, camera);
     }
     render();
-
     function updateCube() {
-        const newCubeGeometry = new THREE.BoxGeometry(Number(widthInput.value), Number(heightInput.value), Number(depthInput.value));
+        const newCubeGeometry = new THREE.BoxGeometry(
+            Number(widthInput.value),
+            Number(heightInput.value),
+            Number(depthInput.value)
+        );
+
         cube.geometry.dispose(); // Dispose of the old geometry
         cube.geometry = newCubeGeometry;
-        cube.material.color.set(colorInput.value);
 
+        const newMaterial = new THREE.MeshLambertMaterial({
+            color: colorInput.value
+        });
+
+        // Update the material for all faces of the cube
+        cube.material = newMaterial;
+
+
+        ////this is the function to animate the generated cube by rotating it a bit, everytime it is generated
+        // Set the initial and target rotation values
+        const startRotation = { x: 0, y: 0 };
+        const targetRotation = { x: Math.PI / 6, y: Math.PI / 4 };
+
+        const duration = 1000; // Animation duration in milliseconds
+        let startTime = null;
+
+        function animateInitialRotation(timestamp) {
+            if (!startTime) startTime = timestamp;
+
+            const progress = timestamp - startTime;
+            const t = Math.min(progress / duration, 1); // Calculate the progress of the animation as a value between 0 and 1
+
+            cube.rotation.x = startRotation.x + (targetRotation.x - startRotation.x) * t;
+            cube.rotation.y = startRotation.y + (targetRotation.y - startRotation.y) * t;
+
+            if (progress < duration) {
+                requestAnimationFrame(animateInitialRotation);
+            }
+        }
+
+        requestAnimationFrame(animateInitialRotation);
 
     }
 
@@ -315,6 +436,9 @@ function initializeScene() {
         event.preventDefault();
     });
 }
+
+
+
 
 // Attempt to initialize the scene immediately
 if (typeof THREE === 'undefined') {
